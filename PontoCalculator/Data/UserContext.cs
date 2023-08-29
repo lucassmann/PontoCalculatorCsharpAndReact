3dbb44969484be2c7ac059a00c4d2a89cb101c60
lucassmann <lucasassmann@outlook.com>
2023-08-29 20:27:16 -0300

    Configured for docker dev env and db

--- PontoCalculator/Data/UserContext.cs
+++ PontoCalculator/Data/UserContext.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using PontoCalculator.Models;
 
 namespace PontoCalculator.Data
 {
     public class UserContext : DbContext
     {
-        public UserContext(DbContextOptions<UserContext> options) : base(options) 
-        { 
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            try
+            {
+                var databaseCreater = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreater != null)
+                {
+                    if (!databaseCreater.CanConnect())
+                    {
+                        databaseCreater.Create();
+                    }
+                    if (!databaseCreater.HasTables())
+                    {
+                        databaseCreater.CreateTables();
+                    }
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public DbSet<User> Users { get; set; }
