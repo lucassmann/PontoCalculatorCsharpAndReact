ef575dd2766fd4dc8aa78c51e8002ed1ba488007
lucassmann <lucasassmann@outlook.com>
2023-11-18 22:08:25 -0300

    Changes email to send the token instead of the web page link

--- PontoCalculator/Dtos/EmailDto.cs
+++ PontoCalculator/Dtos/EmailDto.cs
@@ -4,6 +4,6 @@
     {
         public string To { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
-        public string Link { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
     }
 }
